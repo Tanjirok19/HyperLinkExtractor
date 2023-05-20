7db972e97b7adc4f18b96d5bfd3e34975378f92c
Tanjirok19 <125996668+Tanjirok19@users.noreply.github.com>
2023-05-21 02:15:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 # Your app routes and logic here
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
