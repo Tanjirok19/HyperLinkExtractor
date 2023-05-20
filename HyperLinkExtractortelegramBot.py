3854a306c807897fea7c44f665d4258d0de661d0
Tanjirok19 <125996668+Tanjirok19@users.noreply.github.com>
2023-05-21 01:57:10 +0530

    Update HyperLinkExtractortelegramBot.py

--- HyperLinkExtractortelegramBot.py
+++ HyperLinkExtractortelegramBot.py
@@ -1,16 +1,4 @@
 from telegram.ext import Updater, MessageHandler, Filters
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-# Your app routes and logic here
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
 
 # Define a function to handle incoming messages
 def extract_hyperlink(update, context):
