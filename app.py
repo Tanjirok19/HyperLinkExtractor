a5d92513279036a8d9c1a3a44d227f94ac96cb48
Tanjirok19 <125996668+Tanjirok19@users.noreply.github.com>
2023-05-21 10:53:55 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# Your app routes and logic here
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
